04c3a9bc2554efb1f65ee0efdb154723bba3a714
Yashprit Singh <yashprits@gmail.com>
2015-04-15 23:46:06 +0530

    updating package

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 'use strict';
-module.exports = function() {
 
-};
+var deepFind = function(obj, path) {
+  if (typeof path === 'string') {
+    path = path.split('.');
+  }
+
+  if (typeof path === 'array') {
+    path = path;
+  }
+
+  for (var i = 0; i < path.length; i++) {
+    if ((/\[[0-9]]/).test(path[i])) {
+      var keyIndex = path[i].split('[')
+      obj = obj[keyIndex[0]][parseInt(keyIndex[1], 10)]
+    } else {
+      obj = obj[path[i]];
+    }
+
+    if (!obj) {
+      return false;
+    }
+  }
+  return obj;
+}
+
+module.exports = deepFind;
--- test.js
+++ test.js
@@ -1,1 +1,70 @@
 'use strict';
+
+var assert = require('assert');
+var deepFind = require('./');
+
+it("should except string as path", function() {
+  var obj = {
+    employee: {
+      name: {
+        first: 'yash',
+        last: 'singh'
+      }
+    }
+  }
+
+  var value = deepFind(obj, 'employee.name.first');
+  assert.equal('yash', value)
+});
+
+it("should except array as path", function() {
+  var obj = {
+    employee: {
+      name: {
+        first: 'yash',
+        last: 'singh'
+      }
+    }
+  }
+
+  var value = deepFind(obj, ['employee', 'name', 'first']);
+  assert.equal('yash', value)
+});
+
+it("should handle array as value", function() {
+  var obj = {
+    employees: [{
+      name: {
+        first: 'yash',
+        last: 'singh'
+      }
+    }, {
+      name: {
+        first: 'ruby',
+        last: 'singh'
+      }
+    }]
+  }
+
+  var value = deepFind(obj, ['employees[0]', 'name', 'first']);
+  assert.equal('yash', value)
+});
+
+it("should handle array as value", function() {
+  var obj = {
+    employees: [{
+      name: {
+        first: 'yash',
+        last: 'singh'
+      }
+    }, {
+      name: {
+        first: 'ruby',
+        last: 'singh'
+      }
+    }]
+  }
+
+  var value = deepFind(obj, 'employees[0].name.first');
+  assert.equal('yash', value)
+});
